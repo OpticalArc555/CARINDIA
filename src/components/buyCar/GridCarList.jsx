ff2159cbbb62683edbe227c1187240ce3a16208c
geeteshkumbalkar28 <geeteshkumbalkar@gmail.com>
2024-05-22 23:28:23 +0530

    o

--- src/components/buyCar/GridCarList.jsx
+++ src/components/buyCar/GridCarList.jsx
@@ -27,7 +27,9 @@ console.log(page)
       const nextPage = page + 1; // Increment page here
       console.log(nextPage)
       const res = await fetch(
-        `https://dynamic-expression-production.up.railway.app/cars/mainFilter/${nextPage}`
+        // `https://dynamic-expression-production.up.railway.app/cars/mainFilter/${nextPage}`
+        `http://localhost:8080/cars/mainFilter/${nextPage}`
+
       );
       const data = await res.json();
       console.log(data);
--- src/services/apiSlice.js
+++ src/services/apiSlice.js
@@ -8,7 +8,9 @@ import Cookies from "js-cookie";
 export const apiSlice = createApi({
   reducerPath: "authApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://dynamic-expression-production.up.railway.app/",
+    // baseUrl: "https://dynamic-expression-production.up.railway.app/",
+    baseUrl: "http://localhost:8080/",
+
     prepareHeaders: (headers, { getState }) => {
       // console.log(headers, "prepareHeaders");
       // console.log(getState().auth.token);

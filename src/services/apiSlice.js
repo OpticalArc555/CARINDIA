f3b7e9bff131fe53415b0452a0cfb19d85bbcb49
geeteshkumbalkar028 <geeteshkumbalkar@gmail.com>
2024-06-21 00:23:54 +0530

    ok

--- src/services/apiSlice.js
+++ src/services/apiSlice.js
@@ -8,7 +8,7 @@ import Cookies from "js-cookie";
 export const apiSlice = createApi({
   reducerPath: "authApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://cf0-production.up.railway.app/",
+    baseUrl: "https://cf-production.up.railway.app/",
     prepareHeaders: (headers, { getState }) => {
       // console.log(headers, "prepareHeaders");
       // console.log(getState().auth.token);

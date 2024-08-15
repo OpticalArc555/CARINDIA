cbe2d4650c86f31d6c7714810f9b4166366172c7
Geetesh Kumbalkar <geetesh.kumbalkar@wittygreens.com>
2024-08-15 16:05:43 +0530

    ok

--- src/Utiles/WebSocketConnection.jsx
+++ src/Utiles/WebSocketConnection.jsx
@@ -17,7 +17,7 @@ export const WebSocketProvider = ({ children }) => {
   const subscriptions = useRef({});
 
   useEffect(() => {
-    const socket = new SockJS('https://cffffftasting-production.up.railway.app/Aucbidding');
+    const socket = new SockJS('https://cf-production.up.railway.app/Aucbidding');
     const stompClient = new Client({
       webSocketFactory: () => socket,
       debug: (str) => {
